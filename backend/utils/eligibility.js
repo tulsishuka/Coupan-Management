bd887f6f631992c8e1130b9fa98afcd8d7af31f4
Tulasi <tulsishuklag@gmail.com>
2025-11-26 12:53:23 +0530

    add POST /coupons endpoint to create new coupons

--- backend/utils/eligibility.js
+++ backend/utils/eligibility.js
@@ -1,65 +1,5 @@
-// function checkEligibility(coupon, user, cartValue, cartItems) {
-//   const e = coupon.eligibility || {};
-
-//   // User tier
-//   if (e.allowedUserTiers && !e.allowedUserTiers.includes(user.userTier)) {
-//     return false;
-//   }
-
-//   // Country
-//   if (e.allowedCountries && !e.allowedCountries.includes(user.country)) {
-//     return false;
-//   }
-
-//   // min lifetime spend
-//   if (e.minLifetimeSpend && user.lifetimeSpend < e.minLifetimeSpend) {
-//     return false;
-//   }
-
-//   // min orders placed
-//   if (e.minOrdersPlaced && user.ordersPlaced < e.minOrdersPlaced) {
-//     return false;
-//   }
-
-//   // first order only
-//   if (e.firstOrderOnly && user.ordersPlaced !== 0) {
-//     return false;
-//   }
-
-//   // min cart value
-//   if (e.minCartValue && cartValue < e.minCartValue) {
-//     return false;
-//   }
-
-//   // applicable categories
-//   if (e.applicableCategories) {
-//     const found = cartItems.some((i) =>
-//       e.applicableCategories.includes(i.category)
-//     );
-//     if (!found) return false;
-//   }
-
-//   // excluded categories
-//   if (e.excludedCategories) {
-//     const blocked = cartItems.some((i) =>
-//       e.excludedCategories.includes(i.category)
-//     );
-//     if (blocked) return false;
-//   }
-
-//   // min items count
-//   if (e.minItemsCount) {
-//     const totalQty = cartItems.reduce((sum, i) => sum + i.quantity, 0);
-//     if (totalQty < e.minItemsCount) return false;
-//   }
-
-//   return true;
-// }
-
-// module.exports = { checkEligibility };
 function checkEligibility(coupon, user, cartValue, cartItems) {
   const e = coupon.eligibility || {};
-
   if (e.allowedUserTiers && !e.allowedUserTiers.includes(user.userTier)) return false;
   if (e.allowedCountries && !e.allowedCountries.includes(user.country)) return false;
   if (e.minLifetimeSpend && user.lifetimeSpend < e.minLifetimeSpend) return false;
@@ -81,5 +21,4 @@ function checkEligibility(coupon, user, cartValue, cartItems) {
 
   return true;
 }
-
 module.exports = { checkEligibility };
